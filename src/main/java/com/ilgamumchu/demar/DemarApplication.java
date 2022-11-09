3db3e3826769c53b9a2a78fcbae5413fbef6a24e
unknown <dragontaek98@naver.com>
2022-11-09 10:31:31 +0900

    feat: initial commit

---
+++ src/main/java/com/ilgamumchu/demar/DemarApplication.java
@@ -0,0 +1,13 @@
+package com.ilgamumchu.demar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ilgamumchu/demar/DemarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilgamumchu.demar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
