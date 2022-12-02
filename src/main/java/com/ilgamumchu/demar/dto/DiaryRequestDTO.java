2383fc3ab48ecb6f18687ccb85d049a567dab192
Taejun Moon <junall10022@gmail.com>
2022-12-02 20:20:48 +0900

    feat: diary creation

---
+++ src/main/java/com/ilgamumchu/demar/dto/DiaryRequestDTO.java
@@ -0,0 +1,35 @@
+package com.ilgamumchu.demar.dto;
+
+import com.ilgamumchu.demar.domain.Diary;
+import com.ilgamumchu.demar.domain.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class DiaryRequestDTO {
+
+    private Long id;
+    private User userId;
+    private String title;
+    private String content;
+
+    @Builder
+    public DiaryRequestDTO(User userId, String title, String content) {
+        this.userId = userId;
+        this.title = title;
+        this.content = content;
+    }
+
+    public Diary toEntity(){
+        return Diary.builder()
+                .user_id(this.userId)
+                .title(this.title)
+                .content(this.content)
+                .build();
+    }
+}
